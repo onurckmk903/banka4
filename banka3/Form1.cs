1b541c5676d5cb7fb5932efd087d95cf99e258f3
onurckmk903 <onurcakmak903@gmail.com>
2024-05-15 23:37:33 +0300

    Add project files.

---
+++ banka3/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace banka3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("server=DESKTOP-BVGAO0O ; initial catalog=Mobilbankacılık; integrated security = sspi");
+        public static string adSoyad = "";
+        public static int mID;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButton2.Checked = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kAdi=textBox1.Text;
+            string parola=textBox2.Text;
+            bool sonuc = false;
+            if (radioButton1.Checked)
+            {
+                if (kAdi =="admin"&& parola == "123")
+                {
+                    Yetkiliislem yi=new Yetkiliislem();
+                    yi.Show();
+                    this.Hide();
+                }
+            }
+            else
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from musteriler where tcNo = @p1 and sifre = @p2", con);
+                cmd.Parameters.AddWithValue("p1",kAdi);
+                cmd.Parameters.AddWithValue("p2", parola);
+                
+                SqlDataReader dr=cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    adSoyad = dr["adSoyad"].ToString();
+                    mID =int.Parse( dr["ID"].ToString() ); 
+                    sonuc= true;
+                } 
+              con.Close();
+                if (sonuc)
+                {
+                    sonuc = false;
+                    Müsteriislem mi = new Müsteriislem();
+                    mi.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Hatalı kullanıcı adı/tc veya parola !","hatalı giriş denemesi");
+                }
+
+
+            }
+           
+              
+            
+            
+         
+        }
+    }
+}
---
+++ banka3/Müsteriislem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace banka3
+{
+    public partial class Müsteriislem : Form
+    {
+        public Müsteriislem()
+        {
+            InitializeComponent();
+        }
+    }
+}
